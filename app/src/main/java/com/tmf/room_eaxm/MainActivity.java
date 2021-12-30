f20caa530458e17dc64817e457a76251b77e9c5c
tmfrlck <skkcha@naver.com>
2021-12-30 15:30:27 +0900

    백그라운드 스레드 적용

--- app/src/main/java/com/tmf/room_eaxm/MainActivity.java
+++ app/src/main/java/com/tmf/room_eaxm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tmf.room_eaxm;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,24 +21,31 @@ public class MainActivity extends AppCompatActivity {
         mTodoEditText = findViewById(R.id.todo_edit);
         mResultTextView = findViewById(R.id.result_text);
 
-        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
-                .allowMainThreadQueries()// 지금은 Main Thread에서 동작하도록 허용하지만, 실무에선 background thread에서 동작하도록 작성하도록.
-                .build();// db는 무조건 background에서 동작하지 않으면 에러 발생.
-
         // UI 갱신
-//        db.todoDao().getAll().observe(this, new Observer<List<Todo>>() {
-//            @Override
-//            public void onChanged(List<Todo> todos) {
-//            }
-//        });
+        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db").build();
+
         db.todoDao().getAll().observe(this, todos -> {
             mResultTextView.setText(todos.toString());
         });
 
         // 버튼 클릭 시 DB에 insert
         findViewById(R.id.add_button).setOnClickListener(view -> {
-            db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
+            new InsertAsyncTask(db.todoDao()).execute(new Todo(mTodoEditText.getText().toString()));
         });
 
+    }
+
+    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void> {
+        private TodoDao mTodoDao;
+
+        public InsertAsyncTask(TodoDao todoDao) {
+            this.mTodoDao = todoDao;
+        }
+
+        @Override
+        protected Void doInBackground(Todo... todos) {
+            mTodoDao.insert(todos[0]);
+            return null;
+        }
     }
 }
