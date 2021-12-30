42a035e774fd1773d1a3cfd5cf9a392ae4a211fa
tmfrlck <skkcha@naver.com>
2021-12-30 11:41:31 +0900

    Initial commit

---
+++ app/src/main/java/com/tmf/room_eaxm/TodoDao.java
@@ -0,0 +1,24 @@
+package com.tmf.room_eaxm;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface TodoDao {
+    @Query("SELECT * FROM Todo")
+    List<Todo> getAll();
+
+    @Insert
+    void insert(Todo todo);
+
+    @Update
+    void update(Todo todo);
+
+    @Delete
+    void delete(Todo todo);
+}
